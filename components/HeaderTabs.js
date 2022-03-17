65fd350e60dd24cbc67409e0138f63f9d73f9af2
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-03-17 12:10:17 +0000

    create header section

---
+++ components/HeaderTabs.js
@@ -0,0 +1,54 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState("Delivery");
+  const tabs = ["Delivery", "Pickup"];
+
+  const HeaderBtn = ({ title, onPress }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: activeTab === title ? "#000" : "#fff",
+          paddingHorizontal: 20,
+          paddingVertical: 6,
+          borderRadius: 20,
+          margin: 1,
+        }}
+        onPress={onPress}
+      >
+        <Text
+          style={{
+            color: activeTab === title ? "#fff" : "#000",
+            fontWeight: "800",
+            fontSize: 14,
+          }}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      {tabs.map((tab, index) => {
+        return (
+          <HeaderBtn
+            title={tab}
+            key={index}
+            onPress={() => {
+              setActiveTab(currentTab);
+            }}
+            activeTab={activeTab}
+          />
+        );
+      })}
+    </View>
+  );
+}
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import HeaderTabs from "../components/HeaderTabs";
+
+
+export default function Home() {
+  return (
+    <View>
+      <HeaderTabs />
+    </View>
+  );
+}
